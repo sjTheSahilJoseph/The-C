179d85dddefc1b00b2e17e95a060337115a4e50b
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-13 18:11:42 +0500

    Introduction && .gitignore

--- 00_Introduction/01_Introduction.c
+++ 00_Introduction/01_Introduction.c
@@ -2,6 +2,33 @@
 
 int main(int argc, char const* argv[]) {
     printf("Introduction\n");
+    
+    // Program
+    // A program is a set of instructions which we give to the computer to perform a specific task.
+    
+    // System Program - Program for System Specific Tasks
+    //  For Example [Operating System, File Explorer, Terminal]
+
+    // Application Program - Program for Ourself
+    //  For Example [Browser, Game, Text Editor]
+
+    // The Language
+    // Computer works on Binary, but we can't write programs by writting binary by ourself, It will be quite tedious and time taking thing.
+    // That's why we use a programming language.
+    // When we write a program using a programming language, later on, when we execute the executable, It'll be converted into Binary.
+    // So we solved both problems, one is writing programs, second, a way to write program.
+    
+    // C
+    // Developed by Dennis Ritchie in 1972.
+    // Standardized by ANSI in 1989.
+
+    // We will write our program in C.
+    // The compiler of C will generate an executable file.
+    // Executable file is binary file.
+    // Which we can run.
+
+    // Extension
+    // The extension of C program is ".c".
 
     return 0;
 }
