6807489e964f6a9ee7b0c45e229ecf60ba65ce04
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-14 15:56:37 +0500

    Features of C

---
+++ 03_Features_of_C/main.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+    printf("Features of C Language\n");
+
+    /*
+        Some features of C
+            1. It's a high level programming language.
+                You just have to write code, not binary.
+                We don't have to worry about CPU low level stuff directly.
+            2. It's a small language
+                Just 32 keywords
+            3. It's a core language of many other languages.
+            4. It's portable.
+                Write once, compile everywhere with some modifications if needed.
+            5. Builtin functions.
+                We can use them.
+            6. It's a structured language and moduler.
+                We can arrange the code in very structured manner.
+            7. It supports the use of Pointers.
+                Direct RAM stuff.
+            8. Extensive Language.
+                We can add our own functions to C Library.
+            9. Super FAST.
+            10. Dynammic Memory Allocation.
+                We can use dynamic memory (HEAP).
+            11. It's case sensitive.
+            12. It's use for Embedded System programming.
+    */
+
+    // The compiled file is platform dependant.
+
+    return 0;
+}
