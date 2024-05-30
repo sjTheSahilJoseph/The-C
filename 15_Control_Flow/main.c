c6f8b6a017328013cbe8acbf2618dc3374b0deb4
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-31 01:56:33 +0500

    If

---
+++ 15_Control_Flow/main.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+
+	// Control Flow
+
+	// Control flow tells the program to do things if certain condition is true.
+	// Basically for desicion making, we use control flow.
+	// We sue if, if-else, if-else if, if else ladder, switch-case statements for that.
+
+	if (1) {
+		// any value except 0 is considered as true.
+	}
+	else {
+		// else will execute if if condition is false.
+	}
+
+	// We can add as many if else as we want.
+	// Even we can nest them.
+
+	// Else is optional.
+
+	// We can use else if
+
+	if (0) {
+
+	} else if (0) {
+
+	} else {
+
+	}
+
+
+	// Condition can be anything, any non-zero expression = true, 0 = false.
+
+	// If we are writing one line, then we don't have write block {}, but if more than one line,
+	// we have to write {}.
+
+	// 
+
+	return 0;
+}
